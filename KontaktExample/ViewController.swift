4dfad1b7488206e847a8aaffde32bb64854937b7
Day, Jeff @ Dallas <jeff.day@cbre.com>
2019-01-22 11:37:16 -0600

    Initial Commit

---
+++ KontaktExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KontaktExample
+//
+//  Created by Day, Jeff @ Dallas on 1/22/19.
+//  Copyright © 2019 Day, Jeff @ Dallas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
